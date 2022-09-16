162ef78181900e7f2759b0d705f1982ce94da5dd
Sean Narron <seannarron9@gmail.com>
2022-09-16 17:17:39 +0900

    Created a display chat state.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react'
 import io from "socket.io-client" 
+import Chat from './Chat';
 
 function App() {
 
@@ -9,27 +10,46 @@ function App() {
 
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const [displayChat, setDisplayChat] = useState("");
+  const [displayChat, setDisplayChat] = useState(false);
 
   const joinRoom = () => {
     socket.emit("join_room", room);
+    setDisplayChat(true)
   }
 
 
   return (
     <div className="App">
+
+      {displayChat ? null : 
       
-      <div className="login">
+      
+        <div className="login">
 
-        <label htmlFor="">Name:</label>
-        <input type="text" onChange={(e) => setName(e.target.value)}/><br />
+          <label htmlFor="">Name:</label>
+          <input type="text" onChange={(e) => setName(e.target.value)}/><br />
 
-        <label htmlFor="">Room:</label>
-        <input type="text" onChange={(e) => setRoom(e.target.value)}/><br />
+          <label htmlFor="">Room:</label>
+          <input type="text" onChange={(e) => setRoom(e.target.value)}/><br />
 
-        <button onClick={joinRoom}>JOIN</button>
+          <button onClick={joinRoom}>JOIN</button>
 
-      </div>
+        </div>
+      }
+      
+      
+
+      {displayChat ? 
+
+        <div className="chat">
+          <Chat socket={socket} name={name} room={room}/>
+        </div>
+
+        : null
+      
+      }
+
+      
 
     </div>
   );
